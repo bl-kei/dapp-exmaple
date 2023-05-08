8e784f191a2961217d3ff8c81f42670af6b75a6d
kei <0x4861@beeglelabs.com>
2023-05-09 07:59:55 +0900

    feat : element

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Course from './jsx/Course';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Course />
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
