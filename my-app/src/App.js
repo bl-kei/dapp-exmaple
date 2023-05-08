080211cad9306890563a192138414002eb19a64a
kei <0x4861@beeglelabs.com>
2023-05-09 07:40:28 +0900

    feat : create react project and add component

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Course from './jsx/Course';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Course />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/jsx/Course.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Subject from './Subject';
+
+function Course() {
+  return <Subject name="Math" difficulty={3} />;
+}
+
+export default Course;
