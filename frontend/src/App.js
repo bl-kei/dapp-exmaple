bf62e7c601754cd302daf5ba605401541a4e38c0
kei <0x4861@beeglelabs.com>
2023-05-22 11:43:52 +0900

    feat : dapp example
    - use web3-reacct
    - wallet connect
    - contract deploy & interaction with frontend
    - sign & contract call

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { Connect } from './components/Connect';
+import { WalletStatus } from './components/WalletStatus';
+import { SignMessage } from './components/SignMessage';
+import { ContractCall } from './components/ContractCall';
+
+const StyledAppDiv = styled.div`
+  display: grid;
+  grid-gap: 20px;
+`;
+
+function App() {
+  return (
+    <StyledAppDiv>
+      <Connect />
+      <WalletStatus />
+      <SignMessage />
+      <ContractCall />
+    </StyledAppDiv>
+  );
+}
+
+export default App;
